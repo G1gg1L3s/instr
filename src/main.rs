73cf562c788d79d5b26850ce228911be9f5c2b80
G1gg1L3s <nazar.serhiichuk@cossacklabs.com>
2025-11-28 23:55:22 +0200

    Try to parse robjects content.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeSet;
+use std::{collections::BTreeSet, ffi::CStr};
 
 use iced_x86::{Decoder, Instruction, Mnemonic, OpKind, Register};
 use pe_parser::{pe::PortableExecutable, section::SectionHeader};
@@ -74,10 +74,15 @@ impl<'a> SectionData<'a> {
         let index = usize::try_from(addr.0).unwrap() - usize::try_from(self.address.0).unwrap();
         std::ffi::CStr::from_bytes_until_nul(&self.data[index..]).ok()
     }
+
+    fn len(&self) -> usize {
+        self.data.len()
+    }
 }
 
 #[derive(Debug)]
 struct Sections<'a> {
+    image_base: u32,
     text: SectionData<'a>,
     rdata: SectionData<'a>,
     data: SectionData<'a>,
@@ -160,6 +165,7 @@ fn parse_sections<'a>(pe_bytes: &'a [u8], pe: &PortableExecutable) -> Option<Sec
     }
 
     Some(Sections {
+        image_base,
         text: text?,
         rdata: rdata?,
         data: data?,
@@ -228,7 +234,7 @@ fn parse_import_table(
     sections: &Sections<'_>,
     pe: &PortableExecutable,
 ) -> Option<Vec<ImportTableLib>> {
-    let image_base = pe.optional_header_32?.image_base;
+    let image_base = sections.image_base;
     let import_table = pe.optional_header_32?.data_directories.import_table;
     let addr = Addr(image_base + import_table.virtual_address);
 
@@ -384,12 +390,132 @@ fn extract_call_address(instr: &Instruction) -> Option<u64> {
     }
 }
 
+#[derive(Debug)]
+enum RDataObjectKind {
+    Utf8String(String),
+    Utf16String(String),
+    FunctionsRef(Addr),
+    RdataRef(Addr),
+    DataRef(Addr),
+    Const(u32),
+}
+
+#[derive(Debug)]
+struct RDataObject {
+    addr: Addr,
+    kind: RDataObjectKind,
+}
+
+fn read_ascii_string(data: &[u8]) -> Option<(String, usize)> {
+    let mut end = 0;
+    while end < data.len() {
+        if data[end] == 0 {
+            let s = str::from_utf8(&data[..end]).unwrap();
+            if s.len() >= 4 {
+                return Some((s.to_string(), end + 1));
+            } else {
+                return None;
+            }
+        }
+        if !(0x20..=0x7E).contains(&data[end]) {
+            return None;
+        }
+        end += 1;
+    }
+    None
+}
+
+fn read_utf16_string(data: &[u8]) -> Option<(String, usize)> {
+    if data.len() < 2 {
+        return None;
+    }
+    if data[1] != 0 {
+        return None;
+    } // must start with LE ASCII-range wide char
+
+    let mut end = 0;
+    while end + 1 < data.len() {
+        if data[end] == 0 && data[end + 1] == 0 {
+            let units: Vec<u16> = data[..end]
+                .chunks_exact(2)
+                .map(|c| u16::from_le_bytes([c[0], c[1]]))
+                .collect();
+            let s = String::from_utf16(&units).unwrap();
+            if s.len() >= 4 {
+                return Some((s, end + 2));
+            } else {
+                return None;
+            }
+        }
+        if data[end + 1] != 0 {
+            return None;
+        } // only ASCII-range UTF-16 allowed here
+        end += 2;
+    }
+    None
+}
+
+fn collect_rdata_objects(sections: &Sections<'_>) -> Vec<RDataObject> {
+    let mut res = Vec::with_capacity(sections.rdata.len() / 4);
+
+    let mut addr = sections.rdata.address;
+    let mut data = sections.rdata.data;
+
+    while !data.is_empty() {
+        if let Some((s, size)) = read_ascii_string(data) {
+            res.push(RDataObject {
+                addr,
+                kind: RDataObjectKind::Utf8String(s),
+            });
+            // It seems like string have padding after them
+            let size = size.next_multiple_of(4).min(data.len());
+            addr += size as u32;
+            data = &data[size..];
+            continue;
+        }
+
+        // 2. UTF-16 string?
+        if let Some((s, size)) = read_utf16_string(data) {
+            res.push(RDataObject {
+                addr,
+                kind: RDataObjectKind::Utf16String(s),
+            });
+
+            let size = size.next_multiple_of(4).min(data.len());
+            addr += size as u32;
+            data = &data[size..];
+            continue;
+        }
+
+        let u32 = data[..4].try_into().unwrap();
+        let u32 = u32::from_le_bytes(u32);
+
+        let kind = match sections.image_base.checked_add(u32).map(Addr) {
+            Some(addr) if sections.rdata.contains(addr) => RDataObjectKind::RdataRef(addr),
+            Some(addr) if sections.data.contains(addr) => RDataObjectKind::DataRef(addr),
+            // interestingly, functions use virtual address, not RVA
+            _ if sections.text.contains(Addr(u32)) => RDataObjectKind::FunctionsRef(Addr(u32)),
+            _ => RDataObjectKind::Const(u32),
+        };
+
+        res.push(RDataObject { addr, kind });
+
+        data = &data[4..];
+        addr += 4;
+    }
+
+    res
+}
+
 fn main() {
     let data = std::fs::read("../../Barnyard/Barnyard.exe").unwrap();
     let pe = pe_parser::pe::parse_portable_executable(&data).unwrap();
     let binary = parse_binary(&data, &pe).unwrap();
 
     println!("Binary: {:#?}", binary);
+
+    let robjects = collect_rdata_objects(&binary.sections);
+
     return;
 
     let mut decoder = Decoder::with_ip(
