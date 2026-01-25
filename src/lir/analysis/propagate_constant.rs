9ffcb3f055c96b9eedd71885efc6912f0fe181f7
G1gg1L3s <mr.a678i@gmail.com>
2026-01-25 15:23:35 +0200

    lir: Propagate const calculations

--- src/lir/analysis/propagate_constant.rs
+++ src/lir/analysis/propagate_constant.rs
@@ -2,7 +2,7 @@ use crate::lir::{
     analysis::inverse_map,
     func::SsaFunction,
     ins::{BinOp, Ins},
-    value::Value,
+    value::{Imm, Value},
 };
 
 pub fn exec(func: &mut SsaFunction) {
@@ -44,10 +44,69 @@ pub fn exec(func: &mut SsaFunction) {
 
                     continue;
                 }
+
+                &Ins::BinOp {
+                    op,
+                    dst,
+                    lhs: lhs_val_id,
+                    rhs: rhs_val_id,
+                    flags,
+                } => {
+                    let lhs = &func.values[lhs_val_id];
+                    let rhs = &func.values[rhs_val_id];
+                    let (Value::Imm(lhs), Value::Imm(rhs)) = (lhs, rhs) else {
+                        ins_idx += 1;
+                        continue;
+                    };
+
+                    if let Some(result) = compute_const(op, *lhs, *rhs) {
+                        log::trace!("> Optimizing {lhs} {op} {rhs} at {}", block.addr);
+                        block.ins.remove(ins_idx);
+                        func.values[dst] = Value::Imm(result);
+                        func.ins[ins_id] = Ins::Hole;
+                        val_to_ins.insert(dst, inverse_map::ValueSource::Imm(result));
+
+                        if let Some(flags) = flags {
+                            func.values[flags] = Value::Todo;
+                            val_to_ins.remove(&flags);
+                        }
+                    }
+                }
                 _ => {}
             }
 
             ins_idx += 1;
         }
     }
+}
+
+macro_rules! match_bin_imm {
+    (
+        ($lhs:expr, $rhs:expr),
+        ($x:ident, $y:ident) => $body:expr
+    ) => {{
+        match ($lhs, $rhs) {
+            (Imm::U8($x), Imm::U8($y)) => Imm::U8($body),
+            (Imm::U16($x), Imm::U16($y)) => Imm::U16($body),
+            (Imm::U32($x), Imm::U32($y)) => Imm::U32($body),
+            _ => panic!("mismatched immediate sizes"),
+        }
+    }};
+}
+
+fn compute_const(op: BinOp, lhs: Imm, rhs: Imm) -> Option<Imm> {
+    Some(match_bin_imm!((lhs, rhs), (x, y) => match op {
+        BinOp::Add => x.wrapping_add(y),
+        BinOp::Sub => x.wrapping_sub(y),
+        BinOp::Mulu => return None,
+        BinOp::Muls => return None,
+        BinOp::Div => x / y,
+        BinOp::Xor => x ^ y,
+        BinOp::BitAnd => x & y,
+        BinOp::BitOr => x | y,
+        BinOp::ShiftLeft => x << y,
+        BinOp::ShifRight => x >> y,
+        BinOp::ShifArithRight => return None,
+        BinOp::Condition(_condition) => return None,
+    }))
 }
--- src/main.rs
+++ src/main.rs
@@ -105,6 +105,7 @@ fn main() {
         let mut ssa_func = lir::flat::func_from_flat(func, &blocks);
         ssa_func.resolve();
         lir::analysis::reconstruct_comp::exec(&mut ssa_func);
+        lir::analysis::propagate_constant::exec(&mut ssa_func);
         println!("{}", ssa_func.fmt());
     }
 
