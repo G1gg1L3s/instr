8f18818306306a33c3f9bb7ea8d733c19731cc2d
G1gg1L3s <mr.a678i@gmail.com>
2026-02-22 16:06:40 +0200

    flat ir: fldl2e

--- src/flat_ir.rs
+++ src/flat_ir.rs
@@ -2451,6 +2451,7 @@ fn lower_ins(
         Mnemonic::Fldcw => lower_fldcw(ctx, ins),
 
         Mnemonic::Fld1 => lower_fld_const(ctx, ins, 1.0),
+        Mnemonic::Fldl2e => lower_fld_const(ctx, ins, std::f64::consts::E.log2()),
 
         Mnemonic::Fsqrt => lower_funary(ctx, ins, UnOp::Sqrt, FlagxGroup::X87_C1),
         Mnemonic::Fsin => lower_funary(ctx, ins, UnOp::Sin, FlagxGroup::X87_C1_C2),
